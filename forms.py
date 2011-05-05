c06af99c410e6529a128b240813b75a035d341a1
Alex Bencz <abencz@gmail.com>
2011-05-05 09:11:35 -0400

    Cleaned up old Django files.

--- forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class AddTorrentForm(forms.Form):
-    #title = forms.CharField(max_length=200)
-    file = forms.FileField()
--- settings.py
+++
@@ -1,7 +0,0 @@
-import rtorrentpy
-
-#rtorrentpy.server.uri = 'http://192.168.0.1/RPC2'
-
-# httorrent settings file
-rtorrent_rpc = 'http://192.168.0.1/RPC2'
-torrent_dir = '/mnt/high_speed/public/New/'
